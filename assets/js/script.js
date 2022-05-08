521f85c03be91d4892d2bb8784c855e7e4c0ae9e
Mara Dear <dmaragordon@gmail.com>
2022-05-08 16:47:58 -0400

    added first function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,30 @@
-var lowercasePassword = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
 var uppercasePassword = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var lowercasePassword = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
 var numberPassword = ['0','1','2','3','4','5','6','7','8','9'];
 var specialPassword = ['!','@','#','$','%','^','&','*','(',')','-','_','=','+','{','}','?','[',']','|',';',':','/','`','~',];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function createPassword() {
+  var passLength = prompt("How long would you like your password to be? Please choose a number between 8-128.");
+  if (passLength < 8 || passLength > 128) {
+    alert("Please choose a number between 8-128.");
+    return "Looks like there was a mistake. Please choose a number between 8-128.";
+  }
+
+  if (isNaN(passLength)){
+    window.alert("Please choose a number between 8-128.");
+    return "Looks like there was a mistake. Please choose a number between 8-128.";
+  }
+
+  var uppercase = confirm("Use uppercase letters in your password?");
+  var lowercase = confirm("Use lowercase letters in your password?");
+  var numerics = confirm("Use numbers in your password?");
+  var special = confirm("Use special characters in your password?");
+  var password = "";
+  var choose = [];
+}
 
 // Write password to the #password input
 function writePassword() {

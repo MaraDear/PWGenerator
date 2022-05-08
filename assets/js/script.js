8a33aea90ddf623bf2522ef7f7b9c2e388f6a70e
Mara Dear <dmaragordon@gmail.com>
2022-05-08 16:37:39 -0400

    added variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
-// Assignment code here
-
+var lowercasePassword = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var uppercasePassword = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
+var numberPassword = ['0','1','2','3','4','5','6','7','8','9'];
+var specialPassword = ['!','@','#','$','%','^','&','*','(',')','-','_','=','+','{','}','?','[',']','|',';',':','/','`','~',];
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
